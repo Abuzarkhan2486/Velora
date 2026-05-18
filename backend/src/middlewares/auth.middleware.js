3a3f4538f91e5339344187d63a7fa02902e600ec
Your New Username <abuzarkhan2486@gmail.com>
2026-05-18 17:08:53 +0530

    auth-middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,10 +1,46 @@
-const authMiddleware=async(req,res)=>{
+const jwt = require("jsonwebtoken")
+const userModel = require("../models/user.model")
+
+
+
+const authMiddleware=async(req,res,next)=>{
+
     try {
-        let {email}=req.body
+        let {token} = req.cookie
+
+        if(!token){
+            return res.status(300).json({
+                message:"token not verified ",
+                success:false
+            })
+        }
+
+        let decode = await  jwt.verify(token,process.env.JWT_SECRET)
+
+
+        if(!decode){
+            return res.status(300).json({
+                message:"token not verified ",
+                success:false
+            })
+        }
+
+        let user = await userModel.findById(decode.id)
+
+
+
+        req.user= user
+
+        next()
+
     } catch (error) {
         return res.status(500).json({
             message:"internal server error ",
             success:false
         })
     }
+}
+
+module.exports={
+    authMiddleware
 }
